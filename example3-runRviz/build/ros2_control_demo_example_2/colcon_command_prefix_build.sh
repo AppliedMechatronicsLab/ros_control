4f6b6a7dfcede835c09953e9ab90fea826d59061
son6966 <son6966rz@gmail.com>
2024-12-05 16:06:33 +0700

    add robot multi interface

--- example3-runRviz/build/ros2_control_demo_example_2/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/sonvv/ros_control/example3-runRviz/install/ros2_control_demo_description/share/ros2_control_demo_description/package.sh"
