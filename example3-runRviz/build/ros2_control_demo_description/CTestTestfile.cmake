e7e060c15f8e2eed7b9198e963d5d7e073ae308d
son6966 <son6966rz@gmail.com>
2024-12-05 15:11:14 +0700

    add example3

---
+++ example3-runRviz/build/ros2_control_demo_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sonvv/ros_control/example3-runRviz/src/ros2_control_demo_description
+# Build directory: /home/sonvv/ros_control/example3-runRviz/build/ros2_control_demo_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ example3-runRviz/build/ros2_control_demo_example_2/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/sonvv/ros_control/example3-runRviz/install/ros2_control_demo_description/share/ros2_control_demo_description/package.sh"
